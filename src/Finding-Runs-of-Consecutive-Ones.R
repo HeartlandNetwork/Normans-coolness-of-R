63545fe751229f83b77ef18f4fcf52d8bbc4744f
HeartlandNetwork <gareth_rowell@nps.gov>
2024-01-04 11:23:49 -0600

    print statements

--- src/Finding-Runs-of-Consecutive-Ones.R
+++ src/Finding-Runs-of-Consecutive-Ones.R
@@ -20,47 +20,56 @@ findruns <- function(x, k){
   
   n <- length(x)
   runs <- NULL
-  
+  # print("I am here!")
   for(i in 1:(n - k + 1)){
-    
+    print(n)
+    print(k)
+    print(n - k + 1)
+    print("I am here!")
     # need to determine whether all k values 
     # (x[i], x[i + 1], ... x[i + k - 1]) are 1s
     # applying all() tells us if there is a run there...
     
-    if(all(x[1:(i + k - 1)] == 1)) runs <- c(runs, i)
+    
+    # if(all(x[1:(i + k - 1)] == 1)) runs <- c(runs, i)
   }
   
   return(runs)
 }
 
+x <- c(1,0, 0, 1, 1, 1, 0, 1, 1)
 
-findruns <- function(j,k){
+findruns(x, 3)
+
+findruns(x, 2)
+
+findruns(x, 6)
+
+
+
+
+
+
+
+
+
+
+# function examples
+
+# counts the number of odd integers in x
+oddcount <- function(x){ 
+  k <- 0
+  for(n in x ){
+    print("I am here!")
+    if(n %% 2 == 1) k <- k + 1
+  }
   return(k)
 }
 
-x <- c(1)
-y <- c("this is some text")
+x <- c(1, 3, 5)
 
+y <- c(1,2,3,7,9)
 
-findruns(x, y)
+oddcount(x)
 
-
-x <- 2
-
-
-y <- c(1,0, 0, 1, 1, 1, 0, 1, 1)
-
-findruns(y, 3)
-
-findruns(y, 2)
-
-findruns(y, 6)
-
-flag <- print("I am here!")
-
-flag
-
-
-
-
-
+oddcount(y)
