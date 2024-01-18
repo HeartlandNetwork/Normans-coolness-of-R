74d738db765037822f79ee0cab1e01306180dc5d
HeartlandNetwork <gareth_rowell@nps.gov>
2024-01-18 10:56:21 -0600

    discrete time series cont

--- src/Discrete-Valued-Time-Series.R
+++ src/Discrete-Valued-Time-Series.R
@@ -10,34 +10,35 @@
 # of Lake Huron, 1875 - 1972."
 #
 
-library(help = datasets)
+#library(help = datasets)
 
 
 head(LakeHuron)
 
-my_vector = c(2,4,6,8)
-
 summary(LakeHuron)
 
-DataFrame = data.frame(LakeHuron)
-;.
 # Convert to increasing or decreasing water levels represented by 1's and 0's
 
 find_ones <- function(x) { 
   
-  j = length(x) <<<<<<<<<<<<<<<<<<<< issues here!!
+  j <- length(x) 
+  y <- NULL
   
-  for (i in 1:j ) {
+  print(1:j)
+  
+  print("before for...")
+  
+  for (i in 1:(j) ) {
     
-    if ((x[i + 1] - x[i]) > 0) {
-      
-      y[i] = 1
-      
-    }
+  print(c(i,j))
     
-    else y[i] = 0
+  if ((x[i + 1] - x[i]) > 0) {
+      
+      #}
+    
+    #else y[i] = 0
   }
-    return(y[i])
+    #return(y[i])
   
 }
       
